35fac46ef27dc1156c10544b478d08fee6cf34a0
Yasuhiro Nishihara <yasuhiro@YasuMac.local>
2018-09-24 22:47:26 +0900

    eslint --fix

--- src/components/MenuPage.vue
+++ src/components/MenuPage.vue
@@ -8,7 +8,9 @@
     <v-ons-list-title>Onsen UI Essential Links</v-ons-list-title>
     <v-ons-list>
       <v-ons-list-item modifier="chevron" v-for="item in essentialLinks" @click="goTo(item.link)" :key="item.link">
-        <div class="left"><v-ons-icon fixed-width :icon="item.icon"></v-ons-icon></div>
+        <div class="left">
+          <v-ons-icon fixed-width :icon="item.icon"></v-ons-icon>
+        </div>
         <div class="center">{{ item.label }}</div>
       </v-ons-list-item>
     </v-ons-list>
@@ -18,42 +20,42 @@
 <script>
 export default {
   name: 'menu',
-  data () {
+  data() {
     return {
       essentialLinks: [
         {
           label: 'Docs',
           link: 'https://onsen.io/v2/docs/guide/vue/',
-          icon: 'fa-book'
+          icon: 'fa-book',
         },
         {
           label: 'Gitter Chat',
           link: 'https://gitter.im/OnsenUI/OnsenUI',
-          icon: 'fa-commenting'
+          icon: 'fa-commenting',
         },
         {
           label: 'Forum',
           link: 'https://community.onsen.io',
-          icon: 'ion-chatboxes'
+          icon: 'ion-chatboxes',
         },
         {
           label: 'Twitter',
           link: 'https://twitter.com/Onsen_UI',
-          icon: 'fa-twitter'
+          icon: 'fa-twitter',
         },
         {
           label: 'Playground',
           link: 'https://tutorial.onsen.io/',
-          icon: 'fa-graduation-cap'
-        }
-      ]
+          icon: 'fa-graduation-cap',
+        },
+      ],
     };
   },
   methods: {
-    goTo (url) {
+    goTo(url) {
       window.open(url, '_blank');
-    }
-  }
+    },
+  },
 };
 </script>
 

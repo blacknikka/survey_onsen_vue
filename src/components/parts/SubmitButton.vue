063aa5e8dcce080d33f4b8d68aff9f3fc98454c5
Yasuhiro Nishihara <yasuhiro@YasuMac.local>
2018-09-26 23:48:08 +0900

    Add submit button

---
+++ src/components/parts/SubmitButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <section style="margin: 16px">
+    <v-ons-button modifier="large" style="margin: 6px 0" :disabled="IsSubmitOK">Submit</v-ons-button>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    IsSubmitOK() {
+      return true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
